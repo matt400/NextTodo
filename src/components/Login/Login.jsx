05483332bb2cdb6f74d7ba9cde0b2bc8d59397f6
Pavel Kovalchuk <pavelkovalchuk0510@gmail.com>
2025-12-20 22:11:20 +0100

    Add basic login and registration pages UI (no validation and clear React Structure yet)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,33 @@
 import { Link } from 'react-router-dom';
 import './Login.css';
+import './LoginMedia.css';
+import MailIcon from '../../assets/icons/MailIcon.jsx'
+import PasswordIcon from '../../assets/icons/PasswordIcon.jsx'
 
 const Login = () => {
 	return (
 		<div className='login'>
-			<div className='login heading-section'>
+			<div className='heading-section'>
 				<h1 className='login__header'>Welcome Back</h1>
 				<p className='login__text'>Sign in to manage your tasks</p>
 			</div>
 
-			<div className='field login__field'>
+			<div className='field email'>
+				<MailIcon className = "icon"/>
 				<label htmlFor='email'>Email</label>
 				<input
-					className='field'
+					className='email-input'
 					id='email'
 					type='email'
 					placeholder='Enter your email'
 				/>
 			</div>
-			<div className='field login__field'>
+			<div className='field password'>
+				<PasswordIcon className = "icon"/>
 				<label htmlFor='password'>Password</label>
+				<span className='icon'></span>
 				<input
-					className='field'
+					className='password-input'
 					id='password'
 					type='password'
 					placeholder='Enter your password'
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,29 +1,58 @@
-import { Link } from "react-router-dom";
-import "./Registration.css"
+import { Link } from 'react-router-dom';
+import './Registration.css';
+import MailIcon from '../../assets/icons/MailIcon.jsx';
+import PasswordIcon from '../../assets/icons/PasswordIcon.jsx';
 
 const Registration = () => {
-  return (
-    <div className="registration">
-      <h1>Create Account</h1>
-      <p>Sign up to start managing your tasks</p>
+	return (
+		<div className='registration'>
+			<div className='heading-section'>
+				<h1 className='registration__header'>Create Account</h1>
+				<p className='registration__text'>Sign up to get started</p>
+			</div>
 
-      <label htmlFor="name">Name</label>
-      <input id="name" type="text" placeholder="Enter your name" />
+			<div className='field email'>
+				<MailIcon className='icon' />
+				<label htmlFor='email'>Email</label>
+				<input
+					className='email-input'
+					id='email'
+					type='email'
+					placeholder='Enter your email'
+				/>
+			</div>
 
-      <label htmlFor="email">Email</label>
-      <input id="email" type="email" placeholder="Enter your email" />
+			<div className='field password'>
+				<PasswordIcon className='icon' />
+				<label htmlFor='password'>Password</label>
+				<span className='icon'></span>
+				<input
+					className='password-input'
+					id='password'
+					type='password'
+					placeholder='Enter your password'
+				/>
+			</div>
 
-      <label htmlFor="password">Password</label>
-      <input id="password" type="password" placeholder="Create a password" />
+			<div className='field password'>
+				<PasswordIcon className='icon' />
+				<label htmlFor='password'>Confirm Password</label>
+				<span className='icon'></span>
+				<input
+					className='password-input'
+					id='password'
+					type='password'
+					placeholder='Confirm your password'
+				/>
+			</div>
 
-      <button>Create Account</button>
+			<button className='button registration__sign-in-btn'>Create Account</button>
 
-      <p>
-        Already have an account?{" "}
-        <Link to="/login">Sign in</Link>
-      </p>
-    </div>
-  );
+			<Link className='login__registration-link' to='/login'>
+				Already have an account? Sign in
+			</Link>
+		</div>
+	);
 };
 
 export default Registration;
