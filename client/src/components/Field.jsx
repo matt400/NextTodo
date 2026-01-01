245690f7b463eaefc90e5d002790f9091a729382
Pavel Kovalchuk <pavelkovalchuk0510@gmail.com>
2026-01-01 23:13:11 +0100

    feature (Client): Add validation on email and password in Login and Registration components

--- client/src/components/Field.jsx
+++ client/src/components/Field.jsx
@@ -1,13 +1,35 @@
-import '../styles/elements/field.css';
+const Field = ({
+  innerText,
+  Icon,
+  id,
+  type,
+  label,
+  value,
+  onChange,
+  error
+}) => {
+  return (
+    <div className="field">
 
-const Field = ({ innerText, Icon, id, type, label }) => {
-	return (
-		<div className='field'>
-			{Icon && <Icon className='icon' />}
-			<label htmlFor={id}>{label}</label>
-			<input className='input' id={id} type={type} placeholder={innerText} />
-		</div>
-	);
+      <label htmlFor={id}>{label}</label>
+
+      <div className="field-input-wrapper">
+        {Icon && <Icon className="icon" />}
+
+        <input
+          id={id}
+          type={type}
+          placeholder={innerText}
+          value={value}
+          onChange={onChange}
+          className={error ? "input input--error" : "input"}
+        />
+      </div>
+
+      {error && <span className="field-error">{error}</span>}
+    </div>
+  );
 };
+
 
 export default Field;
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Login.css';
 import './Login-media.css';
 import Heading from '../Heading.jsx';
@@ -6,9 +7,63 @@ import MailIcon from '../../assets/icons/MailIcon.jsx';
 import PasswordIcon from '../../assets/icons/PasswordIcon.jsx';
 import Button from '../Button.jsx';
 import Linking from '../Linking.jsx';
-import { Link } from 'react-router-dom';
 
 const Login = () => {
+	const [values, setValues] = useState({
+		email: '',
+		password: '',
+	});
+
+	const [errors, setErrors] = useState({});
+
+	const validateLive = (field, value) => {
+		let message = '';
+
+		if (field === 'email') {
+			if (!value) message = 'Email is required';
+			else if (!/\S+@\S+\.\S+/.test(value))
+				message = 'Enter a correct email address';
+		}
+
+		if (field === 'password') {
+			if (!value) {
+				message = 'Password is required';
+			} else if (value.length < 8) {
+				message = 'Password must be at least 8 characters long';
+			} else if (value.length > 64) {
+				message = 'Password can have a maximum of 64 characters';
+			} else if (!/[a-z]/.test(value)) {
+				message = 'Password must contain at least one lowercase letter';
+			} else if (!/[A-Z]/.test(value)) {
+				message = 'Password must contain at least one uppercase letter';
+			} else if (!/[0-9]/.test(value)) {
+				message = 'Password must contain at least one digit';
+			} else if (!/[!@#$%^&*()_\-+=\[\]{};:\'",.<>/?`~\\|]/.test(value)) {
+				message = 'Password must contain at least one special character';
+			} else if (/\s/.test(value)) {
+				message = 'Password cannot contain spaces';
+			} else {
+				message = '';
+			}
+		}
+
+		setErrors((prev) => ({
+			...prev,
+			[field]: message,
+		}));
+	};
+
+	const handleChange = (e) => {
+		const { id, value } = e.target;
+
+		setValues((prev) => ({
+			...prev,
+			[id]: value,
+		}));
+
+		validateLive(id, value);
+	};
+
 	return (
 		<div className='login'>
 			<Heading title='Welcome Back' text='Sign in to manage your tasks' />
@@ -19,6 +74,9 @@ const Login = () => {
 				id='email'
 				type='email'
 				label='Email'
+				value={values.email}
+				onChange={handleChange}
+				error={errors.email}
 			/>
 
 			<Field
@@ -27,9 +85,13 @@ const Login = () => {
 				id='password'
 				type='password'
 				label='Password'
+				value={values.password}
+				onChange={handleChange}
+				error={errors.password}
 			/>
 
 			<Button inner='Sign in' to='/mainpage' />
+
 			<Linking to='/register' innerText="Don't have an account? Sign up" />
 		</div>
 	);
