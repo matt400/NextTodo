5f3255a1e41373e1bf2682333d927674d502b435
matt400 <frox674@gmail.com>
2026-01-11 13:42:42 +0100

    Server: Finished basic authentication - Add user login & register with guest verification and basic schema   validation * Fix i18 translations not working

--- server/src/app.js
+++ server/src/app.js
@@ -5,35 +5,42 @@ import jwt from "@fastify/jwt";
 
 import prismaPl from "./plugins/prisma.js";
 import i18nPl from "./plugins/i18n.js";
-import customDecoratorsPl from "./plugins/decorators.js";
+import i18nPlR from "./plugins/i18n.reply.js";
 import bcrypt from "./plugins/bcrypt.js";
 
-import authAPI from "./api/auth/routes.js";
-import userAPI from "./api/user.js";
+import authRoutes from "./api/auth/routes.js";
+import userRoutes from "./api/user/routes.js";
 
 export async function buildApp() {
   const fastify = Fastify({ logger: true });
 
   fastify.setErrorHandler((error, request, reply) => {
-    console.error("ERROR:", error); reply.status(500).send(error);
+    console.error("ERROR:", error);
+    reply.status(500).send(error);
   });
 
   // Plugins
   await fastify.register(prismaPl);
   fastify.register(cookie);
   fastify.register(i18nPl);
-  fastify.register(customDecoratorsPl);
+  fastify.register(i18nPlR);
   fastify.register(bcrypt);
 
   await fastify.register(cors, {
     origin: "http://localhost:5173",
   });
 
-  fastify.register(jwt, { secret: "SUPER_SECRET_KEY123" });
+  fastify.register(jwt, {
+    secret: "SUPER_SECRET_KEY123",
+    cookie: {
+      cookieName: "access_token",
+      signed: false,
+    },
+  });
 
   // API
-  fastify.register(authAPI, { prefix: "/api/auth" });
-  fastify.register(userAPI, { prefix: "/api/user" });
+  fastify.register(authRoutes, { prefix: "/api/auth" });
+  fastify.register(userRoutes, { prefix: "/api/user" });
 
   return fastify;
 }
