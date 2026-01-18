4ec4b9c7f99dc4bd7dd75a83066afda7821006c7
m477 <frox674@gmail.com>
2026-01-19 00:13:24 +0100

    Client: Fixed the API paths * Fixed multiple calls to the API in App * Fixed auto navigate to the /login when no access_token is set

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,53 +1,55 @@
-const API_URL = 'http://localhost:3000/api/auth';
+const API_URL = "http://localhost:3000/api";
+const AUTH_URL = API_URL + "/auth";
+const USER_URL = API_URL + "/user";
 
 export async function register(username, email, password, confirm_password) {
-	const res = await fetch(`${API_URL}/register`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		credentials: 'include',
-		body: JSON.stringify({ username, email, password, confirm_password }),
-	});
+  const res = await fetch(`${AUTH_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ username, email, password, confirm_password }),
+  });
 
-	if (!res.ok) {
-		const err = await res.json();
-		throw new Error(err.message || 'Registration failed');
-	}
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message || "Registration failed");
+  }
 
-	return res.json();
+  return res.json();
 }
 
 export async function login(email, password) {
-	const res = await fetch(`${API_URL}/login`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		credentials: 'include',
-		body: JSON.stringify({ email, password }),
-	});
+  const res = await fetch(`${AUTH_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ email, password }),
+  });
 
-	if (!res.ok) {
-		const err = await res.json();
-		throw new Error(err.message || 'Login failed');
-	}
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message || "Login failed");
+  }
 
-	return res.json();
+  return res.json();
 }
 
 export async function getMe() {
-	const res = await fetch(`${API_URL}/me`, {
-		credentials: 'include',
-	});
-
-	if (res.status === 401) return null;
-	return res.json();
+  const res = await fetch(`${USER_URL}/me`, {
+    method: "GET",
+    credentials: "include",
+  });
+  if (res.status != 200) return false;
+  return res.json();
 }
 
 export async function logout() {
-	return fetch(`${API_URL}/logout`, {
-		method: 'POST',
-		credentials: 'include',
-	});
+  return fetch(`${AUTH_URL}/logout`, {
+    method: "POST",
+    credentials: "include",
+  });
 }
