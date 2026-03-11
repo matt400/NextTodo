6d6661b49801f1fb8c7cc24ed887feb387b0a367
Pavel Kovalchuk <pavelkovalchuk0510@gmail.com>
2026-03-11 20:36:27 +0100

    Client: Loader component, setState && useEffect func fix

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import styles from './Loader.module.css';
+
+export const Loader = () => {
+	return (
+		<div className={styles.backdrop}>
+			<span className={styles.loader}></span>
+		</div>
+	);
+};
+
+export default Loader;
